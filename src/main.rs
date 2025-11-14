287a0b579d9fc1a088b732e9bbc3638e68ee84de
Tim Lauridsen <tla@rasmil.dk>
2025-11-14 14:30:20 +0100

    get package pattern from command line

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use dnf5daemon::daemon::DnfDaemon;
 use dnf5daemon::package::get_packages;
 // use env_logger;
 use log::info;
+use std::env;
 use std::error::Error;
 
 // Although we use `tokio` here, you can use any async runtime of choice.
@@ -14,14 +15,17 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     // Setup logging
     env_logger::init();
-    info!("Starting");
-    let dnf_daemon = DnfDaemon::new().await;
-    dnf_daemon.base.read_all_repos().await.ok();
-    let packages = get_packages(&dnf_daemon, &["yum*"]).await;
-    for pkg in packages {
-        println!(" --> Pkg: {} - {}", pkg.nevra, pkg.size);
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        info!("Starting");
+        let dnf_daemon = DnfDaemon::new().await;
+        dnf_daemon.base.read_all_repos().await.ok();
+        let packages = get_packages(&dnf_daemon, &[args[1].as_str()]).await;
+        for pkg in packages {
+            println!(" --> Pkg: {} - {}", pkg.nevra, pkg.size);
+        }
+        info!("Ending");
     }
-    info!("Ending");
 
     Ok(())
 }
