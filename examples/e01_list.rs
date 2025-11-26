9df60557feea17b184174c741ecf3b6cfc85de6e
Tim Lauridsen <tla@rasmil.dk>
2025-11-26 07:40:53 +0100

    add simple example to list packages

---
+++ examples/e01_list.rs
@@ -0,0 +1,21 @@
+use dnf5daemon::DnfDaemon;
+use dnf5daemon::package::get_packages;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    if let Ok(dnf_daemon) = DnfDaemon::new().await {
+        let rc = dnf_daemon.base.read_all_repos().await.ok().unwrap();
+        println!("Read all repos returned: {:?}", rc);
+        let pattern: Vec<String> = vec!["dnf5*".to_owned()];
+        let packages = &get_packages(&dnf_daemon, &pattern, &"all".to_owned())
+            .await
+            .expect("Error in get_packages");
+        for pkg in packages {
+            println!("{:?}", pkg);
+        }
+    } else {
+        println!("Could not connect to dnf5daemon-server");
+    };
+    Ok(())
+}
