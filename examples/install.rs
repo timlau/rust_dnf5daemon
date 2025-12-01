b33076fe9b98f17db863ec6405ee86450d219f84
Tim Lauridsen <tla@rasmil.dk>
2025-12-01 19:33:18 +0100

    improve errors and better output

--- examples/install.rs
+++ examples/install.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<()> {
     if let Ok(dnf_daemon) = DnfDaemon::default().await {
         let _rc = dnf_daemon.base.read_all_repos().await.ok().unwrap();
         let pkgs: Vec<String> = vec!["0xFFFF".to_owned()];
-        println!(" --> Installing packages {:?}", pkgs);
+        println!("->> Installing packages {:?}", pkgs);
         let mut transaction = Transaction::new(&dnf_daemon);
         transaction.install(&pkgs).await.ok();
         match transaction.resolve().await {
--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,21 @@
-use derive_more::{Display, From};
+use derive_more::From;
 use zbus;
 
-#[derive(Debug, Display, From)]
-#[display("{self:?}")]
+#[derive(Debug, From)]
 pub enum Error {
     TransactionNotResolved(String),
     InvalidTransactionAction(String),
     #[from]
     DBus(zbus::Error),
     DnfDaemon(String),
+}
+
+// Note: Implement Display as debug, for Web and app error, as anyway those errors will need to be streamed as JSON probably
+//       to be rendered for the end user.
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
