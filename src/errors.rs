54a8c195e6f29ab27510575e1d456e7047c605ce
Tim Lauridsen <tla@rasmil.dk>
2025-12-02 12:25:04 +0100

    . reorganize a little and add doc strings

--- src/errors.rs
+++ src/errors.rs
@@ -1,22 +1,28 @@
 use derive_more::From;
 
+/// Result type used throughout the dnf5daemon library
+pub type Result<T> = std::result::Result<T, Error>;
+
+// region:    --- Error
+/// Enum representing possible errors in dnf5daemon operations
 #[derive(Debug, From)]
 pub enum Error {
+    /// Error indicating that a transaction has not been resolved
     TransactionNotResolved(String),
+    /// Error indicating an invalid transaction action
     InvalidTransactionAction(String),
+    /// DBus related error
     #[from]
     DBus(zbus::Error),
+    /// Error indicating failure to connect to DnfDaemon
     DnfDaemon(String),
 }
 
-// Note: Implement Display as debug, for Web and app error, as anyway those errors will need to be streamed as JSON probably
-//       to be rendered for the end user.
 impl core::fmt::Display for Error {
     fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
         write!(fmt, "{self:?}")
     }
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
-
 impl std::error::Error for Error {}
+// endregion: --- Error
