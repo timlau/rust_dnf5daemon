7cfb88140a6b0e1b57454e4c91e35f7926b03ca5
Tim Lauridsen <tla@rasmil.dk>
2025-12-02 11:18:05 +0100

    .lint: more clippy fixes

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,22 +6,22 @@ async fn daemon_test() {
     env_logger::init();
     // Check that new session can be opened.
     if let Ok(mut dnf_daemon) = DnfDaemon::default().await {
-        assert_eq!(dnf_daemon.is_connected(), true);
+        assert!(dnf_daemon.is_connected());
         // Check that we can call a method on one of the interfaces
         let rc = dnf_daemon.base.read_all_repos().await.ok().unwrap();
-        assert_eq!(rc, true);
+        assert!(rc);
         // Check that we can get a get some packages, using the high-level API
         let pattern: Vec<String> = vec!["dnf5*".to_owned()];
-        let packages = get_packages(&dnf_daemon, pattern, &"all".to_string())
+        let packages = get_packages(&dnf_daemon, pattern, "all")
             .await
             .expect("Error in get_packages");
         for pkg in &packages {
             println!("{:?}", pkg);
         }
-        assert!(packages.len() > 0);
+        assert!(packages.is_empty());
         // check that we can manually close the session
         dnf_daemon.close().await.unwrap();
-        assert_eq!(dnf_daemon.is_connected(), false);
+        assert!(!dnf_daemon.is_connected());
     } else {
         println!("Error in creating dbus connection");
     };
