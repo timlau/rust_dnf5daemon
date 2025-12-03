8a8f8b2c13d24734b75281cf4d2f96ef73f69eec
Tim Lauridsen <tla@rasmil.dk>
2025-12-03 08:13:34 +0100

    .  transfer ownership, don't borrow

--- examples/list.rs
+++ examples/list.rs
@@ -7,8 +7,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if let Ok(dnf_daemon) = DnfDaemon::default().await {
         let rc = dnf_daemon.base.read_all_repos().await.ok().unwrap();
         println!("Read all repos returned: {:?}", rc);
-        let pattern: Vec<String> = vec!["dnf5*".to_owned()];
-        let packages = &get_packages(&dnf_daemon, &pattern, Scope::All)
+        let pattern: Vec<String> = vec![String::from("dnf5*")];
+        let packages = get_packages(&dnf_daemon, pattern, Scope::All)
             .await
             .expect("Error in get_packages");
         for pkg in packages {
